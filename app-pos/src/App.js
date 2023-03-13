63752157748846664f6b3f8beb1d61c4c29162ae
Harshil-Dalal-5121 <h.chandreshkumar@axelor.com>
2023-03-13 17:56:09 +0530

    Added local Storage

--- app-pos/src/App.js
+++ app-pos/src/App.js
@@ -1,40 +1,52 @@
+import React, { useEffect, useState } from "react";
 import PosNavbar from "./PosNavbar";
 import CardList from "./CardList";
 import Cart from "./Cart";
 import ItemData from "./ItemData";
-import { ToastContainer } from "react-bootstrap";
-import { useState } from "react";
 
+const cartFromLocalStorage = JSON.parse(
+  localStorage.getItem("LocalStorageCart") || "[]"
+);
 function App() {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(cartFromLocalStorage);
 
-  const addToCart = (data) => {
+  const addToCart = (itemData) => {
     setCartItems((cartItems) => {
-      const productInCart = cartItems.find((item) => item.id === data.id);
+      const productInCart = cartItems.find((item) => item.id === itemData.id);
       if (productInCart) {
-        return cartItems.map((item) =>
-          item.id === data.id ? { ...item, quantity: item.quantity + 1 } : item
-        );
+        return cartItems.map((item) => {
+          if (item.id === itemData.id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
       } else {
-        return [...cartItems, { ...data, quantity: 1 }];
+        return [...cartItems, { ...itemData, quantity: 1 }];
       }
     });
   };
+
+  const clearCart = () => {
+    return setCartItems([]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("LocalStorageCart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <>
       <PosNavbar />
       <div className="row">
-        <div className="col-md-8 py-1 ">
+        <div className="col-md-8 py-2">
           <CardList data={ItemData} addToCart={addToCart} />
         </div>
-        <div className="col-md-4 py-1">
-          <Cart cartItems={cartItems} />
+        <div className="col-md-4 py-2">
+          <Cart cartItems={cartItems} onClick={clearCart} />
         </div>
       </div>
-      <div>
-        <ToastContainer position="top-end"></ToastContainer>
-      </div>
+      <div></div>
     </>
   );
 }
